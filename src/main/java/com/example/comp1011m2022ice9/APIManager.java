16e9a124454a7708f33dfb1f0a40f8f8aa1e3672
nikunjpatel9426 <npatel43210@gmail.com>
2022-07-23 08:57:32 -0400

    Apimanager in MovieFromOMDBByTitleAndYear

--- src/main/java/com/example/comp1011m2022ice9/APIManager.java
+++ src/main/java/com/example/comp1011m2022ice9/APIManager.java
@@ -1,5 +1,12 @@
 package com.example.comp1011m2022ice9;
 
+import com.google.gson.Gson;
+
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
 public class APIManager {
     /******************singleton*****************/
     // Step 1. private static instance member variable
@@ -21,4 +28,31 @@ public class APIManager {
     }
 
     /*************************/
+    public Movie getMovieFromOMDBByTitleAndYear(String title, String year) {
+        // Step 1. Construct your URL
+        String APIKey = "769bf72f";
+        title = title.replace(" ", "%20");
+        String URL = "https://www.omdbapi.com/?apikey=" + APIKey + "&t=" + title + "&y=" + year;
+
+        // Step 2. Create a Client Object
+        HttpClient client = HttpClient.newHttpClient();
+
+        // Step 3. Create a Request Object
+        HttpRequest httpRequest = HttpRequest.newBuilder().uri(URI.create(URL)).build();
+
+        try {
+            // Step 4. Create a Response object
+            HttpResponse<String> response = client.send(httpRequest, HttpResponse.BodyHandlers.ofString());
+
+            // Step 5. Create a Gson object
+            Gson gson = new Gson();
+
+            // Step 6. Deserialize the data using the fromJSON method
+            return gson.fromJson(response.body(), Movie.class);
+        } catch (Exception exception) {
+            exception.printStackTrace();
+        }
+
+        return null;
+    }
 }
